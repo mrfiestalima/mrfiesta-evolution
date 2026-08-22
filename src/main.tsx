c7447b0a433876613ad2951abf79cfe4b36489f0
aymred <aymred@gmail.com>
2026-08-21 21:01:02 -0500

    refactor: introduce MR Fiesta living controls system

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import './index.css'
 import './media.css'
 import './event-media.css'
 import './celebration-modal.css'
+import './controls.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
